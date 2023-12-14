c39109569286aae9a81106cca429b0e07ed7ca81
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-12-14 17:26:28 -0300

    feat: styles changes

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import React from "react";
-import "../styles/globals.css";
+import "../_styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Rachai",
